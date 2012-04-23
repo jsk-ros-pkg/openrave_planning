2863330218ba51329feea412136ee0daafd67d45
rosen <rosen@jsk.t.u-tokyo.ac.jp>
2012-04-23 09:41:00 +0000

    collada_joint_publisher now has explicit dependencies to allow for easier loading

--- orrosplanning/collada_joint_publisher.py
+++ orrosplanning/collada_joint_publisher.py
@@ -11,7 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License. 
-import roslib; roslib.load_manifest('orrosplanning')
+import roslib; roslib.load_manifest('geometry_msgs')
+import roslib; roslib.load_manifest('sensor_msgs')
+import roslib; roslib.load_manifest('openrave')
+import roslib; roslib.load_manifest('tf')
 import rospy
 from openravepy import *
 import sensor_msgs.msg
